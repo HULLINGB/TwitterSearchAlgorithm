d22c91f95ed6b180c59b0791e18ed32ab7852dd7
HULLINGB <bradley.hullinger@ucdenver.edu>
2022-11-28 00:32:06 -0800

    Update SearchAlgorithmForTwitter.java

--- SearchAlgorithmForTwitter.java
+++ SearchAlgorithmForTwitter.java
@@ -5,7 +5,6 @@ public class Search{
 	String input = "";
 	//We assume 100 for now because we cannot return 2 billion accounts
 	String[] topResult = new String[100];
-	Hashmap
 	
 	public static void main(String[] args)
 	{
@@ -15,9 +14,12 @@ public class Search{
 		String input = myObj.nextLine();
 		search(input);
 		
-		for(int y = 0; y < topResult.length; y++)
+		//show the top results return from our query statement in our application.
+		//result by account number, assuming the unique number of the list of all
+		//accounts starts at account 0, going to 1, 2, 3, 4......
+		for(int i = 0; i < topResult.length; i++)
 		{
-			
+			System.out.println(topResult[i]);
 		}
 	}
 
@@ -25,26 +27,30 @@ public class Search{
 	{
 		//This will be our 
 		char[] array1 = input.toCharArray();
-		int a = 0;
+		long a = 0;
 		//this will be the number of existing accounts.
 		//The SQL COUNT() function returns the number of rows in a table satisfying the criteria specified 
 		//in the WHERE clause. It sets the number of rows or non NULL column values. 
 		//COUNT() returns 0 if there were no matching rows.
-		//int length = SQLCOUNT();
+		//long datatype goes up to 9,223,372,036,854,775,807
+		long length = SQLCOUNT();
 		SQLDatabase database = new SQLDatabase();
-		String[] array2 = new String[length];
-		
+		//ArrayList will go up to 2,147,483,647 positions, so we will use array list, and it can be sorted easily.
+		ArrayList<String> array = new ArrayList<String>(length);
+			
 		for(int b = 0; b < length; b++)
 		{
 			//here we assume that each entry has a unique account number, starting at 0 or 1, going to infinity.
-			array2[b] = database.rawQuery("SELECT Username FROM Twitter WHERE AccountNum = " + b);
+			array.add(database.rawQuery("SELECT Username FROM Twitter WHERE AccountNum = " + b));
 		}
 		int charsInARow = 0;
-		int[] array3 = new array3[length];
-		for(int i = 0; i < length; i++)
+		int totalInARow = 0;
+		ArrayList<String> array2 = new ArrayList<String>(length);
+		ArrayList<Integer> = array3 = new ArrayList<Integer>(length);
+		for(long i = 0; i < length; i++)
 		{
-			char[] array2Token = array2[i].toCharArray();
-			for(int z = 0; z < array2Token[i].length; z++)
+			char[] array2Token = array2.get(i).toCharArray();
+			for(int z = 0; z < array2Token.length; z++)
 			{
 				if(array1[z] == array2Token[z])
 				{
@@ -52,7 +58,8 @@ public class Search{
 				}
 				if(array1[z] != array2Token[z])
 				{
-					array3[a] = charsInARow;
+					//totalInARow = totalInARow + charsInARow;
+					array3.add(charsInARow);
 					charsInARow = 0;
 				}
 			}
@@ -60,26 +67,27 @@ public class Search{
 		}
 		Map<int, int> map = new HashMap<int, int>();	
 		
-		for(int w = 0; w < array3.length; w++)
+		for(long w = 0; w < array3.size(); w++)
 		{
-			map.put(array3[w], w);
+			map.put(array3.get(w), w);
 		}
-		for(int x = 0; x < array3.length; x++)
+		int space = 0;
+		for(long x = 0; x < array3.size(); x++)
         {
-            for(int y = 0; y < array3.length; y++)
+            for(long y = 0; y < array3.size(); y++)
             {
 				//descending sort
-                if(map[x] > map[y])
+                if(array3.get(x) > array3.get(y))
                 {
-                    space = array3[x];
-                    array3[x] = array3[y];
-                    array3[y] = space;
+                    space = array3.get(x);
+                    array3.get(x) = array3.get(y);
+                    array3.get(y) = space;
                 }
             }
         }
 		for(int p = 0; p < 100; p++;)
 		{
-			topResult[p] = database.rawQuery("SELECT Username FROM Twitter WHERE AccountNumber = " + map.get(array3[p]);
+			topResult[p] = database.rawQuery("SELECT Username FROM Twitter WHERE AccountNumber = " + map.get(array3.get(p));
 		}
 		
 	}
